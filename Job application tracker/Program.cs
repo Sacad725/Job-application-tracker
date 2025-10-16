b9479f7c15f32c818d918714de186333762e5ccf
Sacad elmi <sacadzidan@gmail.com>
2025-10-16 20:17:20 +0200

    lagt swich sats ()

--- Job application tracker/Program.cs
+++ Job application tracker/Program.cs
@@ -15,9 +15,9 @@
 				Console.WriteLine("\n=== JOB APPLICATION TRACKER ===");
 				Console.WriteLine("1) Lägg till ny ansökan");
 				Console.WriteLine("2) Visa alla ansökningar");
-				Console.WriteLine("3) Filtrera efter status (LINQ)");
-				Console.WriteLine("4) Sortera efter datum (LINQ)");
-				Console.WriteLine("5) Visa statistik (LINQ)");
+				Console.WriteLine("3) Filtrera efter status ");
+				Console.WriteLine("4) Sortera efter datum ");
+				Console.WriteLine("5) Visa statistik ");
 				Console.WriteLine("6) Avsluta");
 				Console.Write("Val: ");
 
@@ -31,7 +31,7 @@
 					case "2": manager.ShowAll(); break;
 					case "3": manager.ShowByStatus(); break;
 					case "4": manager.Showbyddate(); break;
-					case "5": manager.s(); break;
+					case "5": manager.show(); break;
 					case "6": körprogrammet = false; break;
 					default: Console.WriteLine("Fel val."); break;
 				}
