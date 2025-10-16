772f94efcbedd2fa62f78768901ace24d4561adf
Sacad elmi <sacadzidan@gmail.com>
2025-10-16 18:55:44 +0200

    lagt till while loop som heter körprogrammet

--- Job application tracker/Program.cs
+++ Job application tracker/Program.cs
@@ -4,13 +4,36 @@
     {
         static void Main(string[] args)
         {
-		
+		//  skapar objekt av jobbmanager 
             Jobmanager manager = new Jobmanager();
 
+			// while lopp som kör igång programmet 
+			bool körprogrammet = true;
 
+			while (körprogrammet)
+			{
+				Console.WriteLine("\n=== JOB APPLICATION TRACKER ===");
+				Console.WriteLine("1) Lägg till ny ansökan");
+				Console.WriteLine("2) Visa alla ansökningar");
+				Console.WriteLine("3) Filtrera efter status (LINQ)");
+				Console.WriteLine("4) Sortera efter datum (LINQ)");
+				Console.WriteLine("5) Visa statistik (LINQ)");
+				Console.WriteLine("6) Avsluta");
+				Console.Write("Val: ");
 
+				string val = Console.ReadLine();
 
+			}
+
+			Console.WriteLine("Program avslutat!");
 		}
 	}
-    }
+}
+
+
+
+
+		
+	
+    
 
