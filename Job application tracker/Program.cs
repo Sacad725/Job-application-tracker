21a5e7b54ec627e2879ef6fcad9286bfd3b60525
Sacad elmi <sacadzidan@gmail.com>
2025-10-16 20:54:38 +0200

    lagt ny rad på meny valet i swich och loop

--- Job application tracker/Program.cs
+++ Job application tracker/Program.cs
@@ -14,11 +14,12 @@
 			{
 				Console.WriteLine("\n=== JOB APPLICATION TRACKER ===");
 				Console.WriteLine("1) Lägg till ny ansökan");
-				Console.WriteLine("2) Visa alla ansökningar");
-				Console.WriteLine("3) Filtrera efter status ");
-				Console.WriteLine("4) Sortera efter datum ");
-				Console.WriteLine("5) Visa statistik ");
-				Console.WriteLine("6) Avsluta");
+				Console.WriteLine("2) uppdatera befinlig ansöka");
+				Console.WriteLine("3) Visa alla ansökningar");
+				Console.WriteLine("4) Filtrera efter status ");
+				Console.WriteLine("5) Sortera efter datum ");
+				Console.WriteLine("6) Visa statistik ");
+				Console.WriteLine("7) Avsluta");
 				Console.Write("Val: ");
 
 				string Userchoice = Console.ReadLine();
@@ -28,11 +29,12 @@
 				switch (Userchoice)
 				{
 					case "1": manager.Addjobb(); break;
-					case "2": manager.ShowAll(); break;
-					case "3": manager.ShowByStatus(); break;
-					case "4": manager.Showbyddate(); break;
-					case "5": manager.show(); break;
-					case "6": körprogrammet = false; break;
+					case "2":manager.UpdateStatus();break;
+					case "3": manager.ShowAll(); break;
+					case "4": manager.ShowByStatus(); break;
+					case "5": manager.Showbyddate(); break;
+					case "6": manager.(); break;
+					case "7": körprogrammet = false; break;
 					default: Console.WriteLine("Fel val."); break;
 				}
 
